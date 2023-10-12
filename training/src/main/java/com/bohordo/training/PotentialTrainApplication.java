d04b78ccc166799d077dae56f8ca3611cc81ea22
Daniel Bohórquez <dbohorquez@exadel.com>
2023-10-11 21:44:35 -0500

    [PT-1] First commit with template project

---
+++ training/src/main/java/com/bohordo/training/PotentialTrainApplication.java
@@ -0,0 +1,13 @@
+package com.bohordo.training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotentialTrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PotentialTrainApplication.class, args);
+	}
+
+}
---
+++ training/src/test/java/com/bohordo/training/PotentialTrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohordo.training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PotentialTrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
